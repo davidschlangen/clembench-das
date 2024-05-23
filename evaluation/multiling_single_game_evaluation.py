37f3102baca861cb32874fcbc86b12136e4434c5
Anne Beyer <anne.beyer@uni-potsdam.de>
2024-05-23 12:49:57 +0200

    refactor multilingual evaluation script

--- evaluation/multiling_single_game_evaluation.py
+++ evaluation/multiling_single_game_evaluation.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import sys
@@ -10,7 +11,13 @@ from clemgame import metrics
 
 
 def create_overview_table(df: pd.DataFrame, categories: list) -> pd.DataFrame:
-    """Create multilingual results as a table."""
+    """
+    Create multilingual results dataframe.
+    :param df: the initial dataframe with all episode scores
+    :param categories: list of columns over which to aggregate scores
+    :return: the aggregated dataframe
+    """
+
     relevant_metrics = [metrics.METRIC_PLAYED, metrics.BENCH_SCORE, "Aborted at Player 1"]
     # BENCH_SCORE for referencegame = success * 100
     scored_df = df[(df.game == 'referencegame') & (df["metric"].isin(relevant_metrics))]
@@ -22,14 +29,19 @@ def create_overview_table(df: pd.DataFrame, categories: list) -> pd.DataFrame:
     df_means = (scored_df.groupby(categories)
                 .mean(numeric_only=True)
                 .reset_index())
+
     # convert to percentages
     aux_ab_p1 = df_means.loc[df_means.metric == "Aborted at Player 1", 'value']
     aux_played = df_means.loc[df_means.metric == metrics.METRIC_PLAYED, 'value']
     aux_aborted = (1-aux_played).to_list()
     df_means.loc[df_means.metric == "Aborted at Player 1", 'value'] = (aux_ab_p1/aux_aborted) * 100
+
     df_means.loc[df_means.metric == metrics.METRIC_PLAYED, 'value'] *= 100
+    # BENCH_SCORE is already success * 100
 
     df_means = df_means.round(2)
+
+    # rename columns
     df_means['metric'].replace(
         {metrics.METRIC_PLAYED: '% Played', metrics.BENCH_SCORE: '% Success (of Played)', "Aborted at Player 1": 'Aborted at Player 1 (of Aborted)'},
         inplace=True)
@@ -46,6 +58,7 @@ def create_overview_table(df: pd.DataFrame, categories: list) -> pd.DataFrame:
 
     return df_results
 
+
 def save_overview_tables_by_scores(df, categories, path, prefix):
      df_played = df[categories + ['% Played']]
      df_played = df_played.pivot(columns='lang', index="model")
@@ -58,27 +71,36 @@ def save_overview_tables_by_scores(df, categories, path, prefix):
      df_clemscore = df[categories + ['clemscore (Played * Success)']]
      df_clemscore = df_clemscore.pivot(columns='lang', index="model")
      save_table(df_clemscore, path, f"{prefix}_by_clemscore")
-#
+
+
 def save_table(df, path: str, file: str):
     # save table
+    df.to_csv(Path(path) / f'{file}.csv')
     # for adapting for a paper
     df.to_latex(Path(path) / f'{file}.tex', float_format="%.2f") # index = False
     # for easy checking in a browser
-    df.to_html(Path(path) / f'{file}.html')# index = False
-    print(f'\n Saved results into {path}/{file}.html and .tex')
+    df.to_html(Path(path) / f'{file}.html')
+    print(f'\n Saved results into {path}/{file}.csv, .html and .tex')
 
 
 if __name__ == '__main__':
-    # TODO: make options command line argument
-    detailed = False
-    result_path = '../results/v1.5_multiling'
-    compare = '../results/v1.5_multiling_liberal_p2' # False
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-p", "--results_path", type=str, default="../results/v1.5_multiling",
+                            help="A relative or absolute path to the results root directory. Default: ../results/v1.5_multiling")
+    arg_parser.add_argument("-d", "--detailed", type=bool, default=False,
+                            help="Whether to create a detailed overview table by experiment. Default: False")
+    arg_parser.add_argument("-c", "--compare", type=str, default="",
+                            help="An optional relative or absolute path to another results root directory to which the results should be compared.")
+    parser = arg_parser.parse_args()
+
+    output_prefix = parser.results_path.split("/")[-1]
 
     # collect all language specific results in one dataframe
-    df_strict = None
-    if compare:
-        df_liberal = None
-    result_dir = Path(result_path)
+    df_lang = None
+    if parser.compare:
+        df_compare = None
+    result_dir = Path(parser.results_path)
     lang_dirs = glob.glob(f"{result_dir}/*/") # the trailing / ensures that only directories are found
     for lang_dir in lang_dirs:
         lang = lang_dir.split("/")[-2]
@@ -87,28 +109,28 @@ if __name__ == '__main__':
         assert Path(raw_file).is_file()
         lang_result = pd.read_csv(raw_file, index_col=0)
         lang_result.insert(0, 'lang', lang)
-        df_strict = pd.concat([df_strict, lang_result], ignore_index=True)
+        df_lang = pd.concat([df_lang, lang_result], ignore_index=True)
 
-        if compare:
-            raw_file = raw_file.replace(result_path.split("/")[-1], compare.split("/")[-1])
+        if parser.compare:
+            raw_file = raw_file.replace(parser.results_path.split("/")[-1], parser.compare.split("/")[-1])
             assert Path(raw_file).is_file()
             lang_result = pd.read_csv(raw_file, index_col=0)
             lang_result.insert(0, 'lang', lang)
-            df_liberal = pd.concat([df_liberal, lang_result], ignore_index=True)
+            df_compare = pd.concat([df_compare, lang_result], ignore_index=True)
 
-    if detailed:
-        categories = ['lang', 'model', 'experiment', 'metric'] #detailed by experiment
-        overview_detailed = create_overview_table(df_strict, categories)
-        save_overview_tables_by_scores(overview_detailed, categories[:-1], result_path, 'results_multiling_by_experiment')
+    if parser.detailed:
+        categories = ['lang', 'model', 'experiment', 'metric'] # detailed by experiment
+        overview_detailed = create_overview_table(df_lang, categories)
+        save_overview_tables_by_scores(overview_detailed, categories[:-1], parser.results_path, f'{output_prefix}_by_experiment')
 
     else:
         categories = ['lang', 'model', 'metric']
-        overview_strict = create_overview_table(df_strict, categories)
-        save_overview_tables_by_scores(overview_strict, categories[:-1], result_path, 'results_multiling')
+        overview_strict = create_overview_table(df_lang, categories)
+        save_overview_tables_by_scores(overview_strict, categories[:-1], parser.results_path, output_prefix)
 
         # sort models within language by clemscore
         sorted_df = overview_strict.sort_values(['lang','clemscore (Played * Success)'],ascending=[True,False])
-        # extract model order by language for rank correaltion analysis
+        # extract model order by language for rank correlation analysis
         model_orders = {}
         languages = sorted_df['lang'].unique()
         for lang in languages:
@@ -116,12 +138,12 @@ if __name__ == '__main__':
             scores = sorted_df.loc[sorted_df.lang == lang, 'clemscore (Played * Success)']
             models_and_scores = list(zip(models.tolist(), scores.tolist()))
             model_orders[lang] = models_and_scores
-        with open(f'{result_path}/model_rankings_by_language.json', 'w', encoding='utf-8') as f:
+        with open(f'{parser.results_path}/model_rankings_by_language.json', 'w', encoding='utf-8') as f:
             json.dump(model_orders, f, ensure_ascii=False)
-        save_table(sorted_df.set_index(['lang', 'model']), result_path, 'results_multiling')
+        save_table(sorted_df.set_index(['lang', 'model']), parser.results_path, output_prefix)
 
-    if compare:
-        overview_liberal = create_overview_table(df_liberal, categories)
+    if parser.compare:
+        overview_liberal = create_overview_table(df_compare, categories)
         # TODO: adapt comparison to new table format (model x score/lang)
         # get intersection of models
         #models = ["fsc-openchat-3.5-0106"] # "command-r-plus", "Llama-3-8b-chat-hf",
